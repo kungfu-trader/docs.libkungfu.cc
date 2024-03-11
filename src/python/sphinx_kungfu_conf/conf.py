ff3b2b938bf6a06656735d19c173eb1db240b558
翟先生 <43307673+Pythonzhai@users.noreply.github.com>
2024-03-11 09:40:31 +0800

    Update conf.py

--- src/python/sphinx_kungfu_conf/conf.py
+++ src/python/sphinx_kungfu_conf/conf.py
@@ -55,7 +55,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-default_dark_mode = False
+default_dark_mode = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -65,8 +65,8 @@ html_logo = path.join(root_dir, "_static", "images", "logo-sssss.png")
 
 html_favicon = path.join(root_dir, "_static", "images", "icon.png")
 
-# navigation_depth 目录最大深度设置,全局
 
+# navigation_depth 目录最大深度设置,全局
 html_theme_options = {
     "logo_only": True,
     "display_version": True,
